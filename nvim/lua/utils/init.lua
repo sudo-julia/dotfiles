b5735ab031144c1afb427450ea41670eabeaf4c0
sudo-julia <jlearning@tuta.io>
2021-08-19 22:49:58 -0700

    add amfora config

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -27,7 +27,7 @@ function utils.iter_dir(fn, dir, args)
 end
 
 function utils.map(mode, lhs, rhs, opts)
-	local options = {noremap = true}
+	local options = {noremap = true, silent = true}
 	if opts then options = vim.tbl_extend("force", options, opts) end
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
